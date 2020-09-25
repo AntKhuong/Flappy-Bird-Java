fbbf84c1f35b446bdc6578553b2c9268e3e5d091
RandomPerson569 <71333971+RandomPerson569@users.noreply.github.com>
2020-09-25 08:50:18 -0500

    Add files via upload

--- JavaFlappyBird.java
+++ JavaFlappyBird.java
@@ -8,6 +8,10 @@ class JavaFlappyBird extends JPanel implements Runnable, KeyListener
 {
 	private boolean jumpKey;
 	
+	public Rectangle bird;
+	
+	public final int WIDTH = 800, HEIGHT = 800;
+	
 	public JavaFlappyBird()
 	{
 		setBackground(Color.WHITE);
@@ -17,29 +21,34 @@ class JavaFlappyBird extends JPanel implements Runnable, KeyListener
 		addKeyListener( this );
 		setFocusable( true );
 		new Thread(this).start();
+		
+		bird = new Rectangle( WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
 	}
 
 	public void paint( Graphics window )
 	{
 		window.setColor(Color.WHITE); window.fillRect( 0,0, 720, 1280);
 		window.setColor(Color.BLACK); window.drawRect( 0,0, 720, 1280);
+		window.setColor(Color.YELLOW); window.fillRect(bird.x, bird.y, bird.width, bird.height);
 
 		window.setColor(Color.BLUE);		
 		
 		if(jumpKey)
 		{
-			
+			bird.y += 3;
 		}					
 	}
 
 	public void keyPressed(KeyEvent e)
-	{   
-		if( e.getKeyCode()  == KeyEvent.VK_SPACE ) { jumpKey = true; }					
+	{  
+		if( e.getKeyCode()  == KeyEvent.VK_SPACE ) { jumpKey = true; }
+		
 	}
 
 	public void keyTyped(KeyEvent e)
 	{
 		keyPressed( e );			
+		bird.y -= 85;
 	}		
 	public void keyReleased(KeyEvent e)
 	{
